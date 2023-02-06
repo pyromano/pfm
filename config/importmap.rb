b20d01bf3ed469955864aa27cf845e1bd689b062
pyroman <pyr0maneit@gmail.com>
2023-02-06 13:36:32 +0200

    fontawesome to the lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,8 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
 pin 'flatpickr' # @4.6.13
-pin "stimulus-chartjs", to: "https://ga.jspm.io/npm:stimulus-chartjs@5.0.0/dist/stimulus-chartjs.mjs"
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
-pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
-pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.2.0/auto/auto.js"
+pin 'stimulus-chartjs', to: 'https://ga.jspm.io/npm:stimulus-chartjs@5.0.0/dist/stimulus-chartjs.mjs'
+pin '@hotwired/stimulus', to: 'https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js'
+pin '@kurkle/color', to: 'https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js'
+pin 'chart.js/auto', to: 'https://ga.jspm.io/npm:chart.js@4.2.0/auto/auto.js'
+pin_all_from 'app/javascript/lib', under: 'lib'
