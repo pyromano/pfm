69d0644b9c98b765c12039cfd9d367b839533d52
pyroman <pyr0maneit@gmail.com>
2023-02-13 11:17:56 +0200

    validate category name in user scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :operations, dependent: :delete_all
   belongs_to :user
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :description, presence: true
   validates :money_flow, inclusion: { in: MoneyFlow::TYPES.values }
 end
